12f85ab938bc305ca6e24da27d3d062f3d312f14
Jeff Berlin <jeff@jbrally.com>
2018-01-10 11:36:01 -0500

    Adding headline font

--- js/angular.js
+++ js/angular.js
@@ -1,8 +1,7 @@
 var app = angular.module('resume', []);
 
 app.controller('myResume', function($scope) {
-	$scope.contact = "[phone, email]";
     $scope.firstName = "Jeff";
     $scope.lastName = "Berlin";
-
+    $scope.headLine = "Web Developer";
 });
