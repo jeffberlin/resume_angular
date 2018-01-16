131b364056aa302c014e4ee4f86328a71ad4dfae
Jeff Berlin <jeff@jbrally.com>
2018-01-16 16:46:54 -0500

    Finishing top section layout

--- js/angular.js
+++ js/angular.js
@@ -4,5 +4,6 @@ app.controller('myResume', function($scope) {
     $scope.firstName = "Jeff";
     $scope.lastName = "Berlin";
     $scope.headLine = "Web Developer";
+    $scope.email = "jeff@jbrally.com";
     $scope.phone = "(302) 463-5129";
 });
