b3c9a2b20df9acc2c9108f4e20465ca07528996c
Jeff Berlin <jeff@jbrally.com>
2018-01-16 18:09:25 -0500

    Fixing JS

--- js/angular.js
+++ js/angular.js
@@ -1,8 +1,7 @@
 var app = angular.module('resume', []);
 
 app.controller('myResume', function($scope) {
-    $scope.firstName = "Jeff";
-    $scope.lastName = "Berlin";
+    $scope.myName = "Jeff" + " " + "Berlin";
     $scope.headLine = "Web Developer";
     $scope.email = "jeff@jbrally.com";
     $scope.phone = "(302) 463-5129";
