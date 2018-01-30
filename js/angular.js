06698e4776e045ce691d99ff89a3df1643732a1d
Jeff Berlin <jeff@jbrally.com>
2018-01-29 20:25:59 -0500

    Impleted JQuery for smooth scroll on nav click

--- js/angular.js
+++ js/angular.js
@@ -1,10 +1,38 @@
 var app = angular.module('resume', []);
 
-app.controller('myResume', function($scope, $anchorScroll, $location) {
-    $scope.myName = "Jeff" + " " + "Berlin";
+app.controller('myResume', function($scope, $document) {
+    //$scope.myName = "Jeff" + " " + "Berlin";
+    $scope.myName = function() {
+    	return "Jeff" + " " + "Berlin"
+    };
     $scope.headLine = "Web Developer";
     $scope.email = "jeff@jbrally.com";
     $scope.phone = "(302) 463-5129";
     $scope.location = "North Myrtle Beach, SC";
 });
 
+/* JQuery code for the smooth scrolling on nav item clicks */
+
+$("#skills-button").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#skills").offset().top
+	}, 'slow')
+})
+
+$("#experience-button").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#experience").offset().top
+	}, 'slow')
+})
+
+$("#education-button").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#education").offset().top
+	}, 'slow')
+})
+
+$("#portfolio-button").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#portfolio").offset().top
+	}, 'slow')
+})
