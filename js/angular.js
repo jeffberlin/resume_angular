396fb8a23dba934ec98c0504a91b0c54a4987135
Jeff Berlin <jeff@jbrally.com>
2019-01-11 09:20:27 -0500

    Updating

--- js/angular.js
+++ js/angular.js
@@ -211,7 +211,7 @@ app.controller('resumeCtrl', function($scope, $document) {
 		},
     {
 			title: 'MyReads React',
-			image: './images/my-reads-screenshot',
+			image: './images/my-reads-screenshot.png',
 			url: './myreads.html',
 			category: ['development'],
 		// next lines are to handle the pop-up modals
